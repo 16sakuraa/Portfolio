60127db5a0d9ca7696bf129c36ee38e24828341a
16sakuraa <64010860@kmitl.ac.th>
2024-01-06 15:33:02 +0700

    Minor Adjustments

--- my-app/src/pages/skills/skills.tsx
+++ my-app/src/pages/skills/skills.tsx
@@ -40,6 +40,7 @@ function Skills(){
                     <div className="flex flex-row w-full mt-[10px] ml-7 font-jose text-white text-2xl mb-[10px]">
                         - A password game made by using HTML and Javascript.<br></br>
                         - My role in this website is creating a Javascript and deploy using GitHub.<br></br>
+                        - Password for completing the game : Sn@kietikulmarsAngola9998Finitecflkumquat
                     </div>
                 </div>
                 <div className="flex flex-row w-full mt-[20px] font-jose text-white text-4xl">
@@ -47,7 +48,7 @@ function Skills(){
                 </div>
                 <div className="flex flex-col items-center justify-center mt-4">
                     {/* <img src="/Image/skills/unityGif.gif" alt="imageAlt" className="w-[720px] rounded-xl" ></img> */}
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/NFNzK5T0_jo?si=ALuYeb9kzwXNZc7W" 
+                    <iframe  width="1080" height="680" src="https://www.youtube.com/embed/NFNzK5T0_jo?si=ALuYeb9kzwXNZc7W" 
                     title="YouTube video player" frameBorder="0" 
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
                     allowFullScreen>
@@ -55,7 +56,7 @@ function Skills(){
                     </iframe>
                 </div>
                 
-                <div className="flex w-full bg-slate-900 bg-opacity-50 mt-[15px] rounded-lg">
+                <div className="flex w-full bg-slate-900 bg-opacity-50 mt-[15px] rounded-lg mb-[20px]">
                     <div className="flex flex-row w-full mt-[10px] ml-7 font-jose text-white text-2xl mb-[10px]">
                         - My personal project.<br></br>
                         - Create by using Unity and C# Script.<br></br>
